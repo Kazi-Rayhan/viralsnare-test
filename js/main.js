192f1af28820f87bab5cc6429ff83bd559f19399
Abdur Rahman <rahmanabdur64870@gmail.com>
2023-06-28 00:13:55 +0600

    navbar and widgets

---
+++ js/main.js
@@ -0,0 +1,5 @@
+document.getElementById("search-button").addEventListener("click", function () {
+  var searchTerm = document.getElementById("search-input").value;
+  // Perform your search logic here
+  console.log("Search term: " + searchTerm);
+});
